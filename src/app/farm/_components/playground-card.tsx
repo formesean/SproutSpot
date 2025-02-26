173ac2ad3ff44756e7a4610812d68bf9b622cab4
formesean <seanaguilar698@gmail.com>
2025-02-26 17:57:17 +0800

    fix: linting errors on build

--- src/app/farm/_components/playground-card.tsx
+++ src/app/farm/_components/playground-card.tsx
@@ -12,22 +12,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "~/components/ui/popover";
-import { Label } from "~/components/ui/label";
-import { GrowthStage } from "~/types/playground.types";
-
-type GridItem = {
-  id: string;
-  cropType: string;
-  cropCount: number;
-  waterLevel: number;
-  moistureLevel: number;
-  growthStage?: string;
-};
+import type { GridItem } from "~/types/grid-item.types";
 
 export default function PlaygroundCard({
   title,
@@ -36,7 +21,7 @@ export default function PlaygroundCard({
   title: "Actual Playground" | "Experimental Playground";
   gridData: GridItem[];
 }) {
-  const getEmojiSize = (growthStage: any) => {
+  const getEmojiSize = (growthStage: string | undefined) => {
     switch (growthStage) {
       case "Seedling":
         return "text-xs";
@@ -75,7 +60,7 @@ export default function PlaygroundCard({
             <div className="m-1 grid grid-cols-4 grid-rows-3">
               <TooltipProvider>
                 {gridData.map((grid) => (
-                  <Tooltip key={grid.id}>
+                  <Tooltip key={grid.id} delayDuration={200}>
                     <TooltipTrigger asChild>
                       <Button
                         className={`m-1 border-[1px] border-black bg-[url('/soil.png')] bg-contain sm:h-16 sm:w-16 md:h-[102px] md:w-[102px]`}
@@ -121,47 +106,6 @@ export default function PlaygroundCard({
                         ))}
                       </div>
                     </TooltipContent>
-
-                    {/* <TooltipContent className="grid bg-white">
-                      <h1 className="font-lg font-semibold leading-none text-[#166534]">
-                        Crop Details
-                      </h1>
-                      <p className="text-sm text-[#15803d]">
-                        See how your crops are doing.
-                      </p>
-
-                      <hr className="border-[#15803d]" />
-
-                      <div className="grid gap-2">
-                        {[
-                          { label: "Crop Type", value: grid.cropType },
-                          {
-                            label: "Crop Count",
-                            value: grid.cropCount,
-                          },
-                          {
-                            label: "Water Level",
-                            value: grid.waterLevel,
-                          },
-                          {
-                            label: "Moisture Level",
-                            value: grid.moistureLevel,
-                          },
-                        ].map(({ label, value }) => (
-                          <div
-                            key={label}
-                            className="m-1 grid grid-cols-3 items-center gap-4"
-                          >
-                            <Label className="font-medium text-[#166534]">
-                              {label}:
-                            </Label>
-                            <p className="text-sm font-light text-[#15803d]">
-                              {value}
-                            </p>
-                          </div>
-                        ))}
-                      </div>
-                    </TooltipContent> */}
                   </Tooltip>
                 ))}
               </TooltipProvider>
