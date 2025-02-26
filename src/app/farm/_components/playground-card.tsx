3ae6c8d86732b42609c05cbe1fc253b77ce03415
formesean <seanaguilar698@gmail.com>
2025-02-26 11:48:14 +0800

    refactor: add crop details to tooltip instead of popover

--- src/app/farm/_components/playground-card.tsx
+++ src/app/farm/_components/playground-card.tsx
@@ -18,6 +18,7 @@ import {
   PopoverTrigger,
 } from "~/components/ui/popover";
 import { Label } from "~/components/ui/label";
+import { GrowthStage } from "~/types/playground.types";
 
 type GridItem = {
   id: string;
@@ -35,6 +36,27 @@ export default function PlaygroundCard({
   title: "Actual Playground" | "Experimental Playground";
   gridData: GridItem[];
 }) {
+  const getEmojiSize = (growthStage: any) => {
+    switch (growthStage) {
+      case "Seedling":
+        return "text-xs";
+      case "Vegetative":
+        return "text-sm";
+      case "Budding":
+        return "text-base";
+      case "Flowering":
+        return "text-lg";
+      case "Fruiting":
+        return "text-xl";
+      case "Mature":
+        return "text-2xl";
+      case "Harvested":
+        return "text-3xl";
+      default:
+        return "text-base";
+    }
+  };
+
   return (
     <div className="m-5">
       <Card className="border-[#15803d]">
@@ -55,61 +77,91 @@ export default function PlaygroundCard({
                 {gridData.map((grid) => (
                   <Tooltip key={grid.id}>
                     <TooltipTrigger asChild>
-                      <div>
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <Button className="m-1 border-[1px] border-black bg-[url('/soil.png')] bg-contain sm:h-16 sm:w-16 md:h-[102px] md:w-[102px]">
-                              {grid.cropType === "Corn" ? "🌽" : "🌱"}
-                            </Button>
-                          </PopoverTrigger>
-                          <PopoverContent className="w-80 border-[#15803d]">
-                            <div className="grid gap-4">
-                              <div className="space-y-2">
-                                <h4 className="font-semibold leading-none text-[#166534]">
-                                  Crop Details
-                                </h4>
-                                <p className="text-sm text-[#15803d]">
-                                  See how your crops are doing.
-                                </p>
-                                <hr className="border-[#15803d]" />
-                              </div>
-                              <div className="grid gap-2">
-                                {[
-                                  { label: "Crop Type", value: grid.cropType },
-                                  {
-                                    label: "Crop Count",
-                                    value: grid.cropCount,
-                                  },
-                                  {
-                                    label: "Water Level",
-                                    value: grid.waterLevel,
-                                  },
-                                  {
-                                    label: "Moisture Level",
-                                    value: grid.moistureLevel,
-                                  },
-                                ].map(({ label, value }) => (
-                                  <div
-                                    key={label}
-                                    className="m-1 grid grid-cols-3 items-center gap-4"
-                                  >
-                                    <Label className="font-medium text-[#166534]">
-                                      {label}:
-                                    </Label>
-                                    <p className="text-sm font-light text-[#15803d]">
-                                      {value}
-                                    </p>
-                                  </div>
-                                ))}
-                              </div>
-                            </div>
-                          </PopoverContent>
-                        </Popover>
-                      </div>
+                      <Button
+                        className={`m-1 border-[1px] border-black bg-[url('/soil.png')] bg-contain sm:h-16 sm:w-16 md:h-[102px] md:w-[102px]`}
+                      >
+                        <span className={getEmojiSize(grid.growthStage)}>
+                          {grid.cropType === "Corn" ? "🌽" : "🌱"}
+                        </span>
+                      </Button>
                     </TooltipTrigger>
-                    <TooltipContent className="bg-[#77b78a]">
-                      <p>🌱 Growth Stage: {grid.growthStage ?? "Seedling"}</p>
+
+                    <TooltipContent className="rounded-md bg-white px-5 py-3 shadow-md">
+                      <h1 className="text-base font-semibold text-[#166534]">
+                        Crop Details
+                      </h1>
+                      <p className="text-sm text-[#15803d]">
+                        See how your crops are doing.
+                      </p>
+
+                      <hr className="my-2 border-[#15803d]" />
+
+                      <div className="space-y-1">
+                        {[
+                          { label: "Crop Type", value: grid.cropType },
+                          { label: "Crop Count", value: grid.cropCount },
+                          {
+                            label: "Growth Stage",
+                            value: grid.growthStage ?? "Seedling",
+                          },
+                          { label: "Water Level", value: grid.waterLevel },
+                          {
+                            label: "Moisture Level",
+                            value: grid.moistureLevel,
+                          },
+                        ].map(({ label, value }) => (
+                          <div key={label} className="flex justify-between">
+                            <span className="font-medium text-[#166534]">
+                              {label}:
+                            </span>
+                            <span className="text-sm text-[#15803d]">
+                              {value}
+                            </span>
+                          </div>
+                        ))}
+                      </div>
                     </TooltipContent>
+
+                    {/* <TooltipContent className="grid bg-white">
+                      <h1 className="font-lg font-semibold leading-none text-[#166534]">
+                        Crop Details
+                      </h1>
+                      <p className="text-sm text-[#15803d]">
+                        See how your crops are doing.
+                      </p>
+
+                      <hr className="border-[#15803d]" />
+
+                      <div className="grid gap-2">
+                        {[
+                          { label: "Crop Type", value: grid.cropType },
+                          {
+                            label: "Crop Count",
+                            value: grid.cropCount,
+                          },
+                          {
+                            label: "Water Level",
+                            value: grid.waterLevel,
+                          },
+                          {
+                            label: "Moisture Level",
+                            value: grid.moistureLevel,
+                          },
+                        ].map(({ label, value }) => (
+                          <div
+                            key={label}
+                            className="m-1 grid grid-cols-3 items-center gap-4"
+                          >
+                            <Label className="font-medium text-[#166534]">
+                              {label}:
+                            </Label>
+                            <p className="text-sm font-light text-[#15803d]">
+                              {value}
+                            </p>
+                          </div>
+                        ))}
+                      </div>
+                    </TooltipContent> */}
                   </Tooltip>
                 ))}
               </TooltipProvider>
